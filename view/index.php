f90be1f2256ba3318c2ca96d454e43f3d364a451
anonimatogb <gabrielcavalcante22@outlook.com>
2026-02-18 18:41:44 -0300

    inicio

---
+++ view/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <img src="img/logo.jpg" alt="logo" class="ira">
+        <nav>
+            <a href="#">Inicio</a>
+            <a href="#sobre">Sobre</a>
+            <a href="#missao">Missão</a>
+            <a href="#visao">Visão</a>
+            <a href="#valores">Valores</a>
+            <a href="#metas">Metas</a>
+            <a href="#local">Localização</a>
+        </nav>
+    </header>
+    <img src="img/img1.png" alt="img1">
+    <section class="sobre">
+        <h2>Sobre</h2>
+        <p>A APVJ (Associação de Judô) é uma entidade dedicada à promoção, ao ensino e ao desenvolvimento do judô como ferramenta de formação esportiva, educacional e social. Por meio da prática do judô, a APVJ busca contribuir para a construção de cidadãos mais disciplinados, responsáveis e conscientes de seu papel na sociedade.</p>
+    </section>
+    <section class="missao">
+        <h2>Missão</h2>
+        <p>Promover o judô de forma inclusiva e acessível, utilizando seus princípios — como respeito, disciplina, autocontrole e perseverança — para o desenvolvimento físico, mental e moral de crianças, jovens e adultos, dentro e fora dos tatames.</p>
+    </section>
+    <section class="visao">
+        <h2>Visão</h2>
+        <p> Ser reconhecida como uma associação de referência na formação de atletas e cidadãos, destacando-se pela excelência técnica, pelos valores humanos e pelo impacto positivo na comunidade.
+        </p>
+    </section>
+    <section class="valores">
+        <h2>Valores</h2>
+        <ul>
+            <li>Respeito e ética</li>
+            <li>Disciplina e responsabilidade</li>
+
+            <li>Inclusão social</li>
+
+            <li>Trabalho em equipe</li>
+
+            <li>Superação e humildade</li>
+        </ul>
+    </section>
+    <section class="metas">
+        <h2>Metas</h2>
+        <ul>
+            <li>
+                Incentivar a prática do judô como esporte e filosofia de vida</li>
+
+            <li>Desenvolver atletas para competições locais, regionais e nacionais</li>
+
+            <li>Oferecer formação contínua a professores e instrutores</li>
+
+            <li>Promover projetos sociais voltados à inclusão e ao bem-estar</li>
+
+            <li>Fortalecer parcerias com escolas, comunidades e instituições públicas e privadas</li>
+
+        </ul>
+
+    </section>
+
+<section class="local">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d461.0460792071899!2d-50.57744621571625!3d-22.415146975831668!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x949504b78b0dc829%3A0x9c0fc8eef96f4929!2sAv.%20Siqueira%20Campos%2C%20615%20-%20Centro%2C%20Paragua%C3%A7u%20Paulista%20-%20SP%2C%2019700-000!5e1!3m2!1spt-BR!2sbr!4v1771448329474!5m2!1spt-BR!2sbr" width="1000" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</section>
+
+
+</body>
+
+</html>
