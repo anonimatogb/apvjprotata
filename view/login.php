d93eea3ed448f1830504e6ac1aedd61619a589b6
anonimatogb <gabrielcavalcante22@outlook.com>
2026-02-19 22:02:38 -0300

    css

--- view/login.php
+++ view/login.php
@@ -10,6 +10,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="css/style1.css">
 </head>
 
 <body>
